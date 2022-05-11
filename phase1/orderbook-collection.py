84b2cb88ad5a18ccb3842fc3468291f3a2d3e20f
이찬호 <chanholee275@gmail.com>
2022-05-11 16:37:11 +0900

    feat: response의 내용을 검증하는 함수 추가
    response에서 있어야하는 필드와 조건을 검수하기 위한 함수 추가

--- phase1/orderbook-collection.py
+++ phase1/orderbook-collection.py
@@ -3,3 +3,12 @@ import requests
 def request_order_book():
     res = requests.get('https://api.bithumb.com/public/orderbook/BTC_KRW/?count=15')
     return res.json()
+
+def is_validate_response(res: requests.Response):
+    keys = res['data'].keys()
+    for i in ['timestamp', 'payment_currency', 'order_currency', 'bids', 'asks']:
+        if not (i in keys):
+            return False
+    if len(res['data']['bids']) != 15 or len(res['data']['asks']) != 15:
+        return False
+    return True
