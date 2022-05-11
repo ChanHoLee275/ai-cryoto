981678f34ab906c261da72a01e04b2e3ea28d43f
이찬호 <chanholee275@gmail.com>
2022-05-11 16:40:25 +0900

    feat: response의 내용을 order book을 위한 문자열로 변경하는 함수 작성
    response의 필드 내용을 csv 형식에 맞추기 위하여 문자열로 변환하는 함수 작성

--- phase1/orderbook-collection.py
+++ phase1/orderbook-collection.py
@@ -1,4 +1,9 @@
+from datetime import datetime
 import requests
+
+def timestamp_to_date(timestamp):
+    date  = datetime.fromtimestamp(int(timestamp)/1000)
+    return str(date)
 
 def request_order_book():
     res = requests.get('https://api.bithumb.com/public/orderbook/BTC_KRW/?count=15')
@@ -12,3 +17,10 @@ def is_validate_response(res: requests.Response):
     if len(res['data']['bids']) != 15 or len(res['data']['asks']) != 15:
         return False
     return True
+
+def create_orderbook(level=15, res={}):
+    sortedBids = sorted(res['data']['bids'], key=lambda order: order['price'])[0:level-1]
+    sortedAsks = sorted(res['data']['asks'], key=lambda order: order['price'])[0:level-1]
+    order_book_bids = list(map(lambda x: x['price'] + ',' + "{:.6f}".format(float(x['quantity'])) + ',' + '1,' + timestamp_to_date(res['data']['timestamp']) ,sortedBids))
+    order_book_asks = list(map(lambda x: x['price'] + ',' + "{:.6f}".format(float(x['quantity'])) + ',' + '0,' + timestamp_to_date(res['data']['timestamp']) ,sortedAsks))
+    return {'bids': order_book_bids, 'asks': order_book_asks}
