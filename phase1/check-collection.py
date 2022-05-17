746f04140743f7f35d351887815292e0a0f925c6
이찬호 <chanholee275@gmail.com>
2022-05-17 14:44:52 +0900

    feat: phase1에서 수집한 정보 csv 확인을 위한 파일 생성
    csv 파일을 읽고 해당 값들이 유의미한지 확인하기 위한 파일 생성

---
+++ phase1/check-collection.py
@@ -0,0 +1,13 @@
+import sys
+import re
+
+if __name__ == "__main__":
+	# check command line is correct
+	if len(sys.argv) > 2:
+		raise "too many arguments"
+	fileName = sys.argv[1]
+	# check fileName is csv file
+	# check file is exist in current folder
+	# check the number of csv file line
+
+	
