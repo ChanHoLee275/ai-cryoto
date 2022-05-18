003eeeddbab18ac107dce56ec58c8b48cda2077f
이찬호 <chanholee275@gmail.com>
2022-05-18 15:44:02 +0900

    feat: 요청을 보내는 시간을 기준으로 timestamp를 설정하도록 변경
    server의 시간을 기준으로 작성하면, 다음날의 폴더에 현재의 결과가 들어가므로, 그 기준을 요청을 보내는 시간으로 함
    딜레이가 발생하는 경우는 어쩔 수 없는 것으로 함

--- phase1/orderbook-collection.py
+++ phase1/orderbook-collection.py
@@ -6,7 +6,7 @@ import os
 log = open('log.txt', 'a')
 
 def timestamp_to_date(timestamp):
-    date  = datetime.fromtimestamp(int(timestamp)/1000)
+    date  = datetime.fromtimestamp(int(timestamp))
     return str(date)
 
 def request_order_book():
@@ -25,11 +25,11 @@ def is_validate_response(res: requests.Response):
         return False
     return True
 
-def create_orderbook(level=15, res={}):
+def create_orderbook(level=15, res={}, timestamp=0):
     sortedBids = sorted(res['data']['bids'], key=lambda order: order['price'])[0:level-1]
     sortedAsks = sorted(res['data']['asks'], key=lambda order: order['price'])[0:level-1]
-    order_book_bids = list(map(lambda x: x['price'] + ',' + "{:.6f}".format(float(x['quantity'])) + ',' + '1,' + timestamp_to_date(res['data']['timestamp']) ,sortedBids))
-    order_book_asks = list(map(lambda x: x['price'] + ',' + "{:.6f}".format(float(x['quantity'])) + ',' + '0,' + timestamp_to_date(res['data']['timestamp']) ,sortedAsks))
+    order_book_bids = list(map(lambda x: x['price'] + ',' + "{:.6f}".format(float(x['quantity'])) + ',' + '1,' + timestamp_to_date(timestamp) ,sortedBids))
+    order_book_asks = list(map(lambda x: x['price'] + ',' + "{:.6f}".format(float(x['quantity'])) + ',' + '0,' + timestamp_to_date(timestamp) ,sortedAsks))
     return {'bids': order_book_bids, 'asks': order_book_asks}
 
 def create_csv_file(filename):
@@ -40,7 +40,7 @@ def create_csv_file(filename):
 
 def write_csv(orderbooks={}, timestamp=0):
 	# write order book in file
-    date = datetime.fromtimestamp(int(timestamp)/1000)
+    date = datetime.fromtimestamp(int(timestamp))
     filename = str(date.year) + '-' + str(date.month) + '-' + str(date.day) + '-bithumb-btc-orderbook.csv'
     if not os.path.exists(filename):
         create_csv_file(filename)
@@ -54,11 +54,13 @@ scheduler = BlockingScheduler()
 
 @scheduler.scheduled_job('interval', seconds=1, id='save_data')
 def main():
+    current_time = datetime.now().timestamp()
     res = request_order_book()
     if not is_validate_response(res) or res['status'] != "0000":
-        log.write("invalid response : " + str(datetime.now()))
-        return 0
-    order_book = create_orderbook(res = res)
-    write_csv(order_book, int(res['data']['timestamp']))
+        log.write("invalid response : " + timestamp_to_date(current_time))
+        return False
+    order_book = create_orderbook(res = res, timestamp=current_time)
+    write_csv(order_book, current_time)
+    return True
 
 scheduler.start()
