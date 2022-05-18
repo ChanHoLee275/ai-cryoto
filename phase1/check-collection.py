2464653f700a5897ecbcd4704d025c9bdfa2c3d0
이찬호 <chanholee275@gmail.com>
2022-05-18 16:47:16 +0900

    feat: 라인 수를 카운팅하는 함수 작성

--- phase1/check-collection.py
+++ phase1/check-collection.py
@@ -1,5 +1,7 @@
 import sys
-import re
+import os
+
+DAY_SECONDS = 86400
 
 if __name__ == "__main__":
 	# check command line is correct
@@ -7,7 +9,17 @@ if __name__ == "__main__":
 		raise "too many arguments"
 	fileName = sys.argv[1]
 	# check fileName is csv file
+	particle = fileName.split(".csv")
+	if particle[-1] != '' or len(particle) == 0:
+		raise "file is not csv"
 	# check file is exist in current folder
+	if not os.path.exists(fileName):
+		raise "file is not exist"
 	# check the number of csv file line
+	count = 0
+	for row in open(fileName):
+		count += 1
+	print("Expected Number of Lines", str(DAY_SECONDS*30))
+	print("Number of Lines", fileName, str(count - 1)) # remove header line
 
 	
